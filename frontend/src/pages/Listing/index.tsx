10a060b2639ab6df8cf66d830e1370f5fc583860
Rennan Araujo <rennansoarescosta@gmail.com>
2022-05-15 18:21:07 -0300

    Add MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,29 @@
-import Pagination from 'components/Navbar/Pagination';
+import Pagination from 'components/Pagination';
+import MovieStars from 'components/MovieStars';
+import MovieScore from 'components/MovieScore';
+import MovieCard from 'components/MovieCard';
 
 function Listing() {
-  return <Pagination />;
+  return (
+    <div>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+      <MovieStars />
+      <MovieScore />
+    </div>
+  );
 }
 
 export default Listing;
